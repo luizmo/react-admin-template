c3d53666ee9ffd5eabce579c624edb7afdf57d6d
Luiz Miguel <luizmiguel.oliveira18@gmail.com>
2020-01-12 16:37:28 -0300

    Estrutura do projeto e configuração de rotas básicas

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const NotFound = () => (
+  <>
+    <h1>NotFound</h1>
+  </>
+);
+
+export default NotFound;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import NotFound from "./NotFound";
+import Home from "./Home";
+import Login from "./Login";
+
+export { Home, Login, NotFound }
